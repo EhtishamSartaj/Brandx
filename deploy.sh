4194f35adfdea6d9bff00261916f7d0bf2387d5c
sham-vm <etxm606@gmail.com>
2022-06-03 10:38:27 +0500

    deploy.sh modified

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 brand=brandx
 
-git clone https://github.com/EhtishamSartaj/Brand.git
+git clone https://github.com/EhtishamSartaj/brand.git
 cd $brand
 sudo rm -rf /var/www/$brand/*.html
 sudo cp -r . /var/www/$brand/
